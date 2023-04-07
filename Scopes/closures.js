86614d7adb98c1378e6d13b9952e8733053e1403
cesar <ces1406@gmail.com>
2023-04-07 12:01:35 -0300

    add closure example

--- Scopes/closures.js
+++ Scopes/closures.js
@@ -6,3 +6,16 @@ function cienMas(x){
         return int+y+x
     }
 }
+
+// otro ejemplo:
+var funAcc;
+{
+    let nombres = ['juan','pedro','luis'];
+    let pos = 0;
+    funAcc = function(){
+        ++pos;
+        return nombres[pos];
+    }
+}
+console.log('funcAcc():',funAcc())
+console.log('funcAcc():',funAcc())
